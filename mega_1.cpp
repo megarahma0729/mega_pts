5179ef6b9f2ad6e87c7a3bcc0d6c8695fb556742
megarahma0729 <111733868+megarahma0729@users.noreply.github.com>
2022-09-19 20:16:09 +0700

    Add files via upload

---
+++ mega_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama:Guntur Putra Suherman"<< endl;
+    cout << "Kelas:X RPL 2"<< endl;
+    cout << "Nilai:80"<< endl;
+}
---
+++ mega_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int r;
+    float phi=3.14,luas;
+
+    cout<< "jari jari lingkaran : ";
+    cin>>r;
+
+    luas=phi*r*r;
+    cout<<"luas lingkaran "<<luas;
+    return 0;
+}
